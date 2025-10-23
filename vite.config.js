fdf608ac27b1f0c76bc7db695d211cae6054219a
Aditya22-smart <adityalakhotia21@gmail.com>
2025-10-23 18:15:32 +0530

    Updated the task

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "|https://github.com/Aditya22-smart/e-plantShopping",
   plugins: [react()],
 })
